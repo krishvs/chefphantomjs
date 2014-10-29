07837a1529771ce190bee46e9a951f3a449e31f1
krishnan <krishnan@freshdesk.com>
2014-10-29 15:52:11 +0530

    adding phantoms recipe

---
+++ phantomjs/recipes/default.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook Name:: Phantomjs
+# Recipe:: default
+# Create a layer with name phantomjs in opswork 
+
+include_recipe "phantomjs::setup"
---
+++ phantomjs/recipes/setup.rb
@@ -0,0 +1,19 @@
+#Installing phantomjs from source in bitbucket
+
+Chef::Log.info("Downloading the phantomjs source from ::: https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-linux-x86_64.tar.bz2")
+remote_file "/usr/local/share/phantomjs-1.9.8-linux-x86_64.tar.bz2" do
+  source "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-linux-x86_64.tar.bz2"
+end
+
+Chef::Log.info("Extracting phantomjs source")
+execute "sudo tar xjf /usr/local/share/phantomjs-1.9.8-linux-x86_64.tar.bz2" do
+  cwd "/usr/local/share"
+end
+
+Chef::Log.info("Installing phantomjs dependencies")
+execute "sudo yum install fontconfig freetype libfreetype.so.6 libfontconfig.so.1 libstdc++.so.6" do
+  cwd "/usr/local/share/phantomjs-1.9.8-linux-x86_64.tar.bz2"
+end
+execute "sudo ln -s /usr/local/share/phantomjs-1.9.8-linux-x86_64/bin/phantomjs /usr/local/share/phantomjs"
+execute "sudo ln -s /usr/local/share/phantomjs-1.9.8-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs"
+execute "sudo ln -s /usr/local/share/phantomjs-1.9.8-linux-x86_64/bin/phantomjs /usr/bin/phantomjs"
